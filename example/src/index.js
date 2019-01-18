9f663b08e085fc3dd05a523f0b253f74c558c66b
electricSemyon <electricSemyon@gmail.com>
2019-01-19 00:46:26 +0800

    initial

---
+++ example/src/index.js
@@ -0,0 +1,35 @@
+import StateMachine from '../../src'
+
+const button = document.querySelector('button')
+const span = document.querySelector('span')
+const stater = new StateMachine('IDLE_STATE')
+
+const buttonTextMappings = {
+    IDLE_STATE: 'Load stuff',
+    LOADING_STATE: 'Loading stuff...',
+    LOADED_STATE: 'Here u go',
+}
+
+stater.addState('IDLE_STATE')
+stater.addState('LOADING_STATE')
+stater.addState('LOADED_STATE')
+stater.addTransition('IDLE_STATE', 'LOADING_STATE', 'load')
+stater.addTransition('LOADING_STATE', 'LOADED_STATE', 'loaded')
+stater.addTransition('LOADED_STATE', 'LOADED_STATE', 'loaded')
+
+const getButtonText = StateMachine
+    .bindMappings(buttonTextMappings)
+    .withDefault('Load stuff')
+
+stater.subscribe(state => {
+    button.textContent = getButtonText(state)
+})
+
+stater.setTrigger('load', () =>
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => (new Promise(resolve => setTimeout(() => resolve(response), 500)))) // little delay to see results
+        .then(response => response.json())
+        .then(data => span.textContent = JSON.stringify(data.title))
+        .then(_ => stater.dispatch('loaded')))
+
+button.addEventListener('click', () => stater.dispatch('load'))
---
+++ src/index.js
@@ -0,0 +1,65 @@
+class StateMachine {
+    constructor(initialState) {
+        this.currentState = initialState
+        this.states = {}
+
+        this.addState(initialState)
+
+        this.observers = []
+
+        this.triggers = {}
+
+        setTimeout(() => this._triggerObservers())
+    }
+
+    addState(stateName) {
+        this.states[stateName] = {
+            on: {}
+        };
+    }
+
+    addTransition(from, to, message) {
+        this.states[from].on[message] = to
+    }
+
+    dispatch(message) {
+        const newState = this._getNewState(message)
+
+        if (!newState)
+            return
+        
+        if (this.triggers[message])
+            this.triggers[message].forEach(cb => cb(message))
+
+        this.currentState = newState
+
+        this._triggerObservers()
+    }
+
+    setTrigger(message, cb) {
+        if (!this.triggers[message])
+            this.triggers[message] = []
+
+        this.triggers[message].push(cb)
+    }
+
+    subscribe(cb) {
+        this.observers.push(cb)
+    }
+
+    _getNewState(message) {
+        return this.states[this.currentState].on[message]
+    }
+
+    _triggerObservers() {
+        this.observers.forEach(cb => cb(this.currentState))
+    }
+}
+
+StateMachine.bindMappings = mappings => ({
+    withDefault(value) {
+        return message => mappings[message] || value
+    }
+})
+
+export default StateMachine
