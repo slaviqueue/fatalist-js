368088ab5a4a9db4a19573d4ea7eb9a68b6aa990
electricSemyon <electricSemyon@gmail.com>
2019-01-28 00:20:35 +0800

    lol, all this time webpack config was broken; fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
     }
-  ]
 }
